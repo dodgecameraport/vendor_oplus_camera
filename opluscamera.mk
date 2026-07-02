63c58ee05d60dbcacd08c1f968b214d774dd7181
Oscar <osm1019@gmail.com>
2026-07-01 21:22:30 -0700

    unset aux camera

--- opluscamera.mk
+++ opluscamera.mk
@@ -27,7 +27,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.oplus.camera.livephoto.support=1 \
     ro.camera.disableHeicUltraHDR=1 \
     oplus.software.camera.10bit=1 \
-    vendor.camera.aux.packagelist=com.oplus.camera \
     ro.oplus.camera.facing.front.need.disable.nfc=1 \
     ro.oplus.camera.portrait.center.switch=oplus.switch.portrait.center \
     ro.oplus.camera.portrait_center.prefix=oplus.portrait.center. \
